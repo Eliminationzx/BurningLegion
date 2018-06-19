31d3f9dfcf3b1120c9dbc66def0593a3615f6817
Iskezil <vingradov.a.v@bk.ru>
2018-06-19 12:01:06 +0300

    Fix flags and add creature npc id 110971, 100559

---
+++ sql/BurningLegion/world/2018_06_19_00_world_npc.sql
@@ -0,0 +1,4 @@
+UPDATE `creature_template` SET `npcflag`=8192 WHERE (`entry`=110971);
+UPDATE `creature_template` SET `npcflag`=8192 WHERE (`entry`=90639);
+INSERT INTO `creature` VALUES (210118241, 110971, 1220, 0, 0, 1, 0, 0, 0, -1, 0, 0, -223.467, 7734.84, 139.19, 3.09529, 300, 0, 0, 87, 0, 0, 8192, 0, 0, 0, 0, '', 0);
+INSERT INTO `creature` VALUES (210118994, 100559, 1220, 0, 0, 1, 0, 0, 0, -1, 0, 0, 3896.76, 3496.99, 800.305, 2.85291, 300, 0, 0, 130624, 0, 0, 8192, 0, 0, 0, 0, '', 0);
