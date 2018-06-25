9a7807f1c15487bf7f2db1fa4af7460740d17067
Eliminationzx <timurdelimin@gmail.com>
2018-06-25 13:13:02 +0700

    Cleanup

--- src/server/scripts/Spells/spell_item.cpp
+++ src/server/scripts/Spells/spell_item.cpp
@@ -37,7 +37,6 @@
 #include "SpellHistory.h"
 #include "SpellMgr.h"
 #include "SpellScript.h"
-#include "TemporarySummon.h"
 
 // Generic script for handling item dummy effects which trigger another spell.
 class spell_item_trigger_spell : public SpellScriptLoader
