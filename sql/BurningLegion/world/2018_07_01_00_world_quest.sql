b52fe401b4e9327252cda715727e79cebf58f735
Rooter <Slipknot500.slipknot@yandex.ru>
2018-07-01 22:55:02 +0300

    fix quest 40221

--- sql/BurningLegion/world/2018_07_01_00_world_quest.sql
+++ sql/BurningLegion/world/2018_07_01_00_world_quest.sql
@@ -38,3 +38,9 @@ INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry
 ---fix quest 38377
 UPDATE `creature_template` SET `KillCredit1`=92742, `AIName`='SmartAI' WHERE (`entry`=91109);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param_string`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES ('91109', '0', '0', '0', '64', '0', '100', '0', '0', '0', '0', '0', '', '33', '92742', '0', '0', '0', '0', '0', '7', '0', '0', '0', '0', '0', '0', '0', 'Kill credit');
+
+---fix quest 40221
+UPDATE `creature_template` SET `npcflag`=16777216 WHERE (`entry`=93489);
+INSERT INTO `npc_spellclick_spells` (`npc_entry`, `spell_id`, `cast_flags`, `user_type`) VALUES ('93489', '185071', '3', '0');
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param_string`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES ('93489', '0', '1', '0', '73', '0', '100', '0', '0', '0', '0', '0', '', '41', '1500', '35', '0', '0', '0', '0', '1', '0', '0', '0', '0', '0', '0', '0', 'Despawn');
+INSERT INTO `spell_linked_spell` (`spell_trigger`, `spell_effect`, `type`, `comment`) VALUES ('185071', '225082', '0', 'Fix quest 40221');
