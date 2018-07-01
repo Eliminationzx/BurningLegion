5470a54110f6b6eeb3945fa923fb6d1bf6ed668a
Rooter <Slipknot500.slipknot@yandex.ru>
2018-07-01 22:38:06 +0300

    Fix quest 38377

--- sql/BurningLegion/world/2018_07_01_00_world_quest.sql
+++ sql/BurningLegion/world/2018_07_01_00_world_quest.sql
@@ -35,4 +35,6 @@ INSERT INTO `waypoints` (`entry`, `pointid`, `position_x`, `position_y`, `positi
 INSERT INTO `waypoints` (`entry`, `pointid`, `position_x`, `position_y`, `position_z`, `point_comment`) VALUES ('10488500', '5', '3129.95', '5915.79', '287.856', '0');
 INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES ('15', '999917', '0', '0', '0', '14', '0', '41890', '0', '0', '1', '0', '0', '', 'Conditions quest 41890');
 
-
+---fix quest 38377
+UPDATE `creature_template` SET `KillCredit1`=92742, `AIName`='SmartAI' WHERE (`entry`=91109);
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param_string`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES ('91109', '0', '0', '0', '64', '0', '100', '0', '0', '0', '0', '0', '', '33', '92742', '0', '0', '0', '0', '0', '7', '0', '0', '0', '0', '0', '0', '0', 'Kill credit');
