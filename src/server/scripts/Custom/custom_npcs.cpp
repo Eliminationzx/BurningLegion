ad7d8366efdbebd7f5e5e3c0541ebedce2f77aa6
Eliminationzx <timurdelimin@gmail.com>
2018-07-05 20:24:40 +0700

    Fix allied races can be also configured for start level

--- src/server/scripts/Custom/custom_npcs.cpp
+++ src/server/scripts/Custom/custom_npcs.cpp
@@ -287,59 +287,8 @@ public:
     }
 };
 
-enum Expnpc
-{
-    GOSSIP_MENU_ID_EXP_NPC = 77713
-};
-
-class Exp_NPC : public CreatureScript
-{
-public:
-    Exp_NPC() : CreatureScript("Exp_NPC") { }
-
-    bool OnGossipHello(Player* player, Creature* creature) override
-    {
-        if (player->IsInCombat())
-            player->SendNotification(LANG_EXP_NPC_COMBAT, CHAT_MSG_RAID_BOSS_EMOTE);
-        else
-        {
-            AddGossipItemFor(player, GOSSIP_MENU_ID_EXP_NPC, 0, GOSSIP_SENDER_MAIN, 1);
-            SendGossipMenuFor(player, player->GetGossipTextId(creature), creature->GetGUID());
-        }
-
-        CloseGossipMenuFor(player);        
-        return true;
-    }
-
-    bool OnGossipSelect(Player* player, Creature* /*creature*/, uint32 /*sender*/, uint32 action) override
-    {
-        ClearGossipMenuFor(player);
-        
-        switch(action)
-        {                                              
-            case 1: 
-            {
-                if (player->getLevel() < 100)
-                {
-                    player->GiveLevel(100);
-                    player->SendNotification(LANG_EXP_NPC_LEVEL_UP, CHAT_MSG_RAID_BOSS_EMOTE);
-
-                }
-                else
-                    player->SendNotification(LANG_EXP_NPC_LEVEL_ERROR, CHAT_MSG_RAID_BOSS_EMOTE);
-
-                break;
-            }
-        }  
-
-        CloseGossipMenuFor(player);
-        return true;
-    } 
-};
-
 void AddSC_custom_npcs()
 {
     new npc_rate_xp_modifier();
     new npc_challenger();
-    new Exp_NPC();
 }
