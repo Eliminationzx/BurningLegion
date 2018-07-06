3bf9af83869fcd4443c9acb2e80df58dd1bda449
Eliminationzx <timurdelimin@gmail.com>
2018-07-06 17:57:23 +0700

    Rewrite SQL query

---
+++ sql/BurningLegion/world/2018_07_06_00_areatrigger_scripts.sql
@@ -0,0 +1,4 @@
+DELETE FROM areatrigger_scripts WHERE entry IN (718, 5484);
+INSERT INTO areatrigger_scripts (entry, ScriptName) VALUES 
+(718, 'at_monk_ring_of_peace'),
+(5484, 'at_monk_song_of_chi_ji');
--- sql/BurningLegion/world/2018_07_06_00_spell_script_names.sql
+++
@@ -1,2 +0,0 @@
-UPDATE `areatrigger_template` SET `ScriptName` = 'at_monk_ring_of_peace' WHERE `Id` = 718;
-UPDATE `areatrigger_template` SET `ScriptName` = 'at_monk_song_of_chi_ji' WHERE `Id` = 5484;
