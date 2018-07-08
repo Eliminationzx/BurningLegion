33d74e42d6f9d76139ca15fecf579f5420c243a5
Eliminationzx <timurdelimin@gmail.com>
2018-07-08 14:36:55 +0700

    typo

--- sql/BurningLegion/world/2018_07_06_00_areatrigger_scripts.sql
+++ sql/BurningLegion/world/2018_07_06_00_areatrigger_scripts.sql
@@ -1,4 +1,4 @@
-DELETE FROM areatrigger_scripts WHERE entry IN (718, 5484);
+DELETE FROM areatrigger_scripts WHERE entry IN (3983, 10191);
 INSERT INTO areatrigger_scripts (entry, ScriptName) VALUES 
-(718, 'at_monk_ring_of_peace'),
-(5484, 'at_monk_song_of_chi_ji');
+(3983, 'at_monk_ring_of_peace'),
+(10191, 'at_monk_song_of_chi_ji');
