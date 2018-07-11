930e07ef35b01ada3457fad83057d814fd522fca
Rooter <Slipknot500.slipknot@yandex.ru>
2018-07-11 14:58:59 +0300

    fix quest 40956

---
+++ sql/BurningLegion/world/2018_07_11_00_world_quest.sql
@@ -0,0 +1,4 @@
+---fix quest 40956
+UPDATE `quest_objectives` SET `Flags`=4 WHERE (`ID`=283885);
+UPDATE `quest_objectives` SET `Flags`=4 WHERE (`ID`=281600);
+UPDATE `quest_objectives` SET ``Flags`=4 WHERE (`ID`=281565);
