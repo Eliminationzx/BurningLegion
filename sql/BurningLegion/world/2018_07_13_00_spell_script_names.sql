2a02481e7fdd7c5c986dfa974f9f71147767137e
Eliminationzx <timurdelimin@gmail.com>
2018-07-14 00:56:58 +0700

    typo

--- sql/BurningLegion/world/2018_07_13_00_spell_script_names.sql
+++ sql/BurningLegion/world/2018_07_13_00_spell_script_names.sql
@@ -5,18 +5,18 @@ DELETE FROM spell_script_names WHERE spell_id = 248580;
 INSERT INTO spell_script_names (spell_id, ScriptName) VALUES (248580, 'spell_war_shattered_defenses');
 
 DELETE FROM spell_linked_spell WHERE spell_effect = -248625;
-INSERT INTO spell_linked_spell (spell_trigger, spell_effect, type, comment) VALUES (12294, -248625, 1, 'Remove shattered defenses buff by mortal strike');
-INSERT INTO spell_linked_spell (spell_trigger, spell_effect, type, comment) VALUES (163201, -248625, 1, 'Remove shattered defenses buff by execute');
+INSERT INTO spell_linked_spell (spell_trigger, spell_effect, type, comment) VALUES (12294, -248625, 0, 'Remove shattered defenses buff by mortal strike');
+INSERT INTO spell_linked_spell (spell_trigger, spell_effect, type, comment) VALUES (163201, -248625, 0, 'Remove shattered defenses buff by execute');
 
 DELETE FROM spell_script_names WHERE spell_id = 251879;
 INSERT INTO spell_script_names (spell_id, ScriptName) VALUES (251879, 'spell_war_t21_arms4p_bonus');
 
 DELETE FROM spell_linked_spell WHERE spell_effect = -253383;
-INSERT INTO spell_linked_spell (spell_trigger, spell_effect, type, comment) VALUES (1680, -253383, 1, 'Remove weighted blade buff by whirlwind');
-INSERT INTO spell_linked_spell (spell_trigger, spell_effect, type, comment) VALUES (1464, -253383, 1, 'Remove weighted blade buff by slam');
+INSERT INTO spell_linked_spell (spell_trigger, spell_effect, type, comment) VALUES (1680, -253383, 0, 'Remove weighted blade buff by whirlwind');
+INSERT INTO spell_linked_spell (spell_trigger, spell_effect, type, comment) VALUES (1464, -253383, 0, 'Remove weighted blade buff by slam');
 
 DELETE FROM spell_script_names WHERE spell_id = 238147;
 INSERT INTO spell_script_names (spell_id, ScriptName) VALUES (238147, 'spell_war_executioners_precision');
 
 DELETE FROM spell_linked_spell WHERE spell_effect = -242188;
-INSERT INTO spell_linked_spell (spell_trigger, spell_effect, type, comment) VALUES (12294, -242188, 1, 'Remove executioners precision buff by mortal strike');
+INSERT INTO spell_linked_spell (spell_trigger, spell_effect, type, comment) VALUES (12294, -242188, 0, 'Remove executioners precision buff by mortal strike');
