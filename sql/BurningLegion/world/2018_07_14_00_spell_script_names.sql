666c123c3762834b57975ce598994ae759656a1f
Eliminationzx <timurdelimin@gmail.com>
2018-07-15 01:54:38 +0700

    typo

--- sql/BurningLegion/world/2018_07_14_00_spell_script_names.sql
+++ sql/BurningLegion/world/2018_07_14_00_spell_script_names.sql
@@ -4,9 +4,7 @@ INSERT INTO spell_script_names (spell_id, ScriptName) VALUES (196923, 'spell_pal
 DELETE FROM spell_script_names WHERE spell_id = 204147;
 INSERT INTO spell_script_names (spell_id, ScriptName) VALUES (204147, 'spell_hun_windburst');
 
-DELETE FROM areatrigger_template WHERE Id = 10713;
-INSERT INTO `areatrigger_template` (`Id`, `Type`, `Flags`, `Data0`, `Data1`, `Data2`, `Data3`, `Data4`, `Data5`, `ScriptName`, `VerifiedBuild`) VALUES 
-('10713','0','0','2','2','0','0','0','0','at_hun_windburst','26124');
+UPDATE areatrigger_template SET ScriptName = 'at_hun_windburst' WHERE Id=10713;
 
 DELETE FROM spell_script_names WHERE scriptname in ("spell_pal_aura_of_sacrifice", "spell_pal_aura_of_sacrifice_ally");
 INSERT INTO spell_script_names VALUES
