5d44c45c40b8cd9c2496f2c5429bfe0f1da0dac0
Rooter <Slipknot500.slipknot@yandex.ru>
2018-07-20 02:29:24 +0300

    Some fix for Argus

---
+++ sql/BurningLegion/world/2018_07_20_00_world_quest.sql
@@ -0,0 +1,16 @@
+---fix quest 47883
+UPDATE `quest_objectives` SET `Flags`=4 WHERE (`ID`=291199);
+UPDATE `quest_objectives` SET `Flags`=4 WHERE (`ID`=291090);
+
+---fix quest 47689
+UPDATE `quest_objectives` `Flags`=4 WHERE (`ID`=290927);
+UPDATE `quest_objectives` SET `Flags`=4 WHERE (`ID`=290951);
+UPDATE `quest_objectives` SET `Flags`=4 WHERE (`ID`=290950);
+UPDATE `quest_objectives` SET `Flags`=4 WHERE (`ID`=290946);
+UPDATE `quest_objectives` SET `Flags`=4 WHERE (`ID`=290926);
+
+---fix quest 47685
+UPDATE `quest_objectives` SET `Flags`=4 WHERE (`ID`=292420);
+UPDATE `quest_objectives` SET `Flags`=4 WHERE (`ID`=290901);
+UPDATE `quest_objectives` SET `Flags`=4 WHERE (`ID`=290757);
+UPDATE `quest_objectives` SET `Flags`=4 WHERE (`ID`=291204);
