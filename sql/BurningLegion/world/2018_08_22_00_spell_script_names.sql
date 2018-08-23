fbc4b6954aa830775bbb45080291ea70b963cfaa
Eliminationzx <timurdelimin@gmail.com>
2018-08-23 12:55:27 +0700

    Fix fiery band dot damage

--- sql/BurningLegion/world/2018_08_22_00_spell_script_names.sql
+++ sql/BurningLegion/world/2018_08_22_00_spell_script_names.sql
@@ -1,4 +1,5 @@
-DELETE FROM `spell_script_names` WHERE `spell_id` IN (5221, 214743);
+DELETE FROM `spell_script_names` WHERE `spell_id` IN (5221, 214743, 207771);
 INSERT INTO `spell_script_names` VALUE
 (5221, "spell_dru_shred"),
+(207771, "spell_dh_fiery_brand_dot"),
 (214743, "spell_dh_soul_carver");
