7d59b4605b0e0b14f07cde785b3c6d211963a861
Eliminationzx <timurdelimin@gmail.com>
2018-08-28 19:50:41 +0700

    Fix build

--- src/server/game/Spells/SpellMgr.cpp
+++ src/server/game/Spells/SpellMgr.cpp
@@ -704,53 +704,50 @@ bool SpellArea::IsFitToRequirements(Player const* player, uint32 newZone, uint32
             break;
         }
         case 240010: //Class Hall Banishing
+        {
+            switch (player->GetAreaId())
             {
-                switch (player->GetAreaId())
-                {
-                    case 7679: //DK Hall
-                        if (player->getClass() == CLASS_DEATH_KNIGHT)
-                            return false;
-                        break;
-                    case 7752: //Shaman Hall
-                    case 7753:
-                        if (player->getClass() == CLASS_SHAMAN)
-                            return false;
-                        break;
-                    case 7813: //Warrior Hall
-                        if (player->getClass() == CLASS_WARRIOR)
-                            return false;
-                        break;
-                    case 7879: //Mage Hall
-                        if (player->getClass() == CLASS_MAGE)
-                            return false;
-                        break;
-                    case 8012: //Rogue Hall
-                        if (player->getClass() == CLASS_ROGUE)
-                            return false;
-                        break;
-                    case 7875: //Warlock Hall
-                        if (player->getClass() == CLASS_WARLOCK)
-                            return false;
-                        break;
-                    case 7903: //Monk Hall
-                        if (player->getClass() == CLASS_MONK)
-                            return false;
-                        break;
-                    case 7834: //Priest Hall
-                    case 8356:
-                        if (player->getClass() == CLASS_PRIEST)
-                            return false;
-                        break;
-                    default:
-                        break;
-                }
-                break;
+                case 7679: //DK Hall
+                    if (player->getClass() == CLASS_DEATH_KNIGHT)
+                        return false;
+                    break;
+                case 7752: //Shaman Hall
+                case 7753:
+                    if (player->getClass() == CLASS_SHAMAN)
+                        return false;
+                    break;
+                case 7813: //Warrior Hall
+                    if (player->getClass() == CLASS_WARRIOR)
+                        return false;
+                    break;
+                case 7879: //Mage Hall
+                    if (player->getClass() == CLASS_MAGE)
+                        return false;
+                    break;
+                case 8012: //Rogue Hall
+                    if (player->getClass() == CLASS_ROGUE)
+                        return false;
+                    break;
+                case 7875: //Warlock Hall
+                    if (player->getClass() == CLASS_WARLOCK)
+                        return false;
+                    break;
+                case 7903: //Monk Hall
+                    if (player->getClass() == CLASS_MONK)
+                        return false;
+                    break;
+                case 7834: //Priest Hall
+                case 8356:
+                    if (player->getClass() == CLASS_PRIEST)
+                        return false;
+                    break;
+                default:
+                    break;
             }
-            default:
-                break;
+            break;
         }
-
-
+        default:
+            break;
     }
 
     return true;
